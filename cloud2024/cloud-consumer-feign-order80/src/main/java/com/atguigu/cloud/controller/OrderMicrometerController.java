4969213d1751f5052d3ad21cf9375b0c17c6fb3b
Gxf <2284467180@qq.com>
2025-06-20 00:13:53 +0800

    gateway配置

--- cloud2024/cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderMicrometerController.java
+++ cloud2024/cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderMicrometerController.java
@@ -29,5 +29,4 @@ public class OrderMicrometerController {
         return payFeignApi.myMicrometer(id);
     }
 
-
 }
---
+++ cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayGateWayController.java
@@ -0,0 +1,45 @@
+package com.atguigu.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import com.atguigu.cloud.entity.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: cloud2024
+ * @BelongsPackage: com.atguigu.cloud.controller
+ * @Author: GuoXiaofeng
+ * @CreateTime: 2025-06-19  23:58
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@Slf4j
+@RestController
+@RequestMapping("/pay/gateway")
+public class PayGateWayController {
+
+    @Resource
+    private PayService payService;
+
+    @GetMapping("/get/{id}")
+    @Operation(summary = "查询单个", description = "查询支付流水, 参数是Id")
+    public ResultData<PayDTO> getById(@PathVariable("id") Integer id) {
+        PayDTO payDTO = new PayDTO();
+        BeanUtils.copyProperties(payService.getById(id), payDTO);
+        return ResultData.success(payDTO);
+    }
+
+    @GetMapping("/getInfo")
+    public ResultData<String> getInfo() {
+        return ResultData.success("这是网关测试: " + IdUtil.simpleUUID());
+    }
+
+}
